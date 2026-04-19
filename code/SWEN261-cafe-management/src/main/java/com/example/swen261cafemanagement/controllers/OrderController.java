67c5fc822cbcbfbbf04bdcc7f63431cd4e5c6ea7
AliHamie5 <145132758+AliHamie5@users.noreply.github.com>
2026-04-19 23:57:35 +0400

    Update OrderController.java

--- code/SWEN261-cafe-management/src/main/java/com/example/swen261cafemanagement/controllers/OrderController.java
+++ code/SWEN261-cafe-management/src/main/java/com/example/swen261cafemanagement/controllers/OrderController.java
@@ -52,7 +52,6 @@ public class OrderController {
         model.addAttribute("hasOrders", !orders.isEmpty());
         model.addAttribute("noOrders", orders.isEmpty());
         model.addAttribute("resultCount", orders.size());
-        model.addAttribute("successMsg", "Status updated successfully");
         
         return "orders";
     }
